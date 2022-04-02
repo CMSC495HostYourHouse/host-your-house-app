6ade182e612fe2590532fee4ebfd55dc4eb8c8e2
Khalil <khalil.james.savoy@gmail.com>
2022-04-02 19:43:05 -0400

    css style changes for app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
-import MainPage from './shared/components/main-page';
+import MainContent from './shared/components/MainContent/main-content';
+import MenuNav from "./shared/components/MenuNav/menu-nav";
+import AppFooter from "./shared/components/AppFooter/app-footer";
+import React from "react";
 
 const App = () => {
   return (
-    <div id={'main-container'} style={{height: "100vh"}}><MainPage /></div>
+    <div id={'main-container'} style={{height: "100vh"}}>
+      <MenuNav/>
+      <MainContent/>
+      <AppFooter/>
+    </div>
+
   );
 }
 
