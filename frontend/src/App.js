75455c11890520ee20e87fa59aff5a17a636b804
Khalil <khalil.james.savoy@gmail.com>
2022-04-02 21:43:39 -0400

    changed landing page to be main page so that we don't require users to sign in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,20 @@
 import React from "react";
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Landing } from './components/Landing';
-import { Register } from './components/Register';
-import { Login } from './components/Login';
-import MainContent from './shared/components/MainContent/main-content';
-import MenuNav from "./shared/components/MenuNav/menu-nav";
-import AppFooter from "./shared/components/AppFooter/app-footer";
+import { Landing } from './shared/components/LandingPage/Landing';
+import { Register } from './shared/components/RegisterPage/Register';
+import { Login } from './shared/components/LoginPage/Login';
+import { MainPage } from './shared/components/main-page/main-page';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={ <Landing/> }/>
-        <Route path="/register" element={ <Register/> }/>
-        <Route path="/login" element={ <Login/> }/>
-      </Routes>
-    </Router>
-
-    // <div id={'main-container'} style={{height: "100vh"}}>
-    //   <MenuNav/>
-    //   <MainContent/>
-    //   <AppFooter/>
-    // </div>
-
+      <Router>
+          <Routes>
+              <Route path="/" element={ <MainPage/> }/>
+              <Route path="/register" element={ <Register/> }/>
+              <Route path="/login" element={ <Login/> }/>
+          </Routes>
+      </Router>
   );
 }
 
