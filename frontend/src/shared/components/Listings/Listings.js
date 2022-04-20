ff784a965aded8752094866dc7cb05ac851c4bd9
ThomasL21 <102611451+ThomasL21@users.noreply.github.com>
2022-04-20 19:34:43 -0400

    Update Listings.js

--- frontend/src/shared/components/Listings/Listings.js
+++ frontend/src/shared/components/Listings/Listings.js
@@ -6,9 +6,23 @@ import PropertyCards from '../PropertyCards/propertyCards';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import MainPageSearch from '../MainPageSearch/mainpageSearch';
+import { response } from 'express';
 
 export const listings = () => {
+    async function getHouse(){
+        await fetch("http://localhost:5000/users", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+          
+        }).catch (error => { //if there is an error
+            window.alert(error); //make a pop-up of the issue appear
+            return; //end the code block
+        });
+    }
      return (
+         
         <section>
             <Container className='d-flex p-2 justify-content-center flex-row'>
                 <Card className='listings-card' bg='dark' text='light'>
