8da6f00fd027e3b3e8fa8e2fbaea712abd2fe526
ThomasL21 <102611451+ThomasL21@users.noreply.github.com>
2022-04-26 14:51:02 -0400

    navbar link fix
    navbar host a house link was linking to /main that was unused since main page is just /

--- frontend/src/shared/components/NavBar/navBar.js
+++ frontend/src/shared/components/NavBar/navBar.js
@@ -10,7 +10,7 @@ export const TopMenu = () => {
      return (
         <section className='navBar'>
             <Navbar bg="dark" expand="md" variant='dark' fixed="top">
-                <Navbar.Brand href="/main">Host A House</Navbar.Brand>
+                <Navbar.Brand href="/">Host A House</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="justify-content-end d-flex pe-5" style={{ width: "100%" }}>
