061e098556214ba7808ef19af834cacbfca0d96a
CalebElkinsDev <CalebElkinsDev@outlook.com>
2022-05-01 13:17:32 -0500

    Restyled NavBar

--- frontend/src/shared/components/NavBar/navBar.js
+++ frontend/src/shared/components/NavBar/navBar.js
@@ -4,6 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import {checkToken} from "../../../utils/authToken";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 // navbar that shows on top of page
 
@@ -16,7 +17,12 @@ export const TopMenu = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="justify-content-end d-flex pe-5" style={{ width: "100%" }}>
-                            <NavDropdown title="Account" id="nav-dropdown" menuVariant='dark' >
+                            {/* Todo: Get name from signed in user */}
+                            <Navbar.Text>Hello, User</Navbar.Text>
+
+                            {/* inline styling to add the profile icon as the title */}
+                            <NavDropdown title={ <div className='nav-dropdown-title' style={{display: "inline-block"}}>
+                                                    <AccountCircleIcon/></div>} id="nav-dropdown" menuVariant='dark'>
                                 <NavDropdown.Item href="/account">My Account</NavDropdown.Item>
                                 <NavDropdown.Item href="/saved">Saved Properties</NavDropdown.Item>
                                 <NavDropdown.Item href="/reserved">Reserved Properties</NavDropdown.Item>
@@ -32,14 +38,11 @@ export const TopMenu = () => {
                 <Navbar bg="dark" expand="md" variant='dark' fixed="top">
                     <Navbar.Brand href="/">Host A House</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="justify-content-end d-flex pe-5" style={{ width: "100%" }}>
-                            <NavDropdown title="Account" id="nav-dropdown" menuVariant='dark' >
-                                <NavDropdown.Item href="/login">Login</NavDropdown.Item>
-                                <NavDropdown.Item href="/register">Register</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
+                    
+                    <Nav className="justify-content-end d-flex pe-5" style={{ width: "100%" }}>
+                        <Nav.Link href="/login">Login</Nav.Link>
+                        <Nav.Link href="/register">Register</Nav.Link>                            
+                    </Nav>
                 </Navbar>
             </section>
         )
